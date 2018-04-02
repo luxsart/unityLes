c3dca56101e45c5df2e9c8fd27387fec0fd03d38
Unknown <laliepopy@hotmail.com>
2018-04-02 21:36:43 +0200

    Beter laat dan nooit

---
+++ Assets/Sripts/Keypad.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keypad : MonoBehaviour
+{
+
+    public string curPassword = "12345";
+    public string input;
+    public bool onTrigger;
+    public bool doorOpen;
+    public bool keypadScreen;
+    public Transform doorHinge;
+    public GameObject bodka;
+    public AudioSource Vrzganie;
+       
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+        keypadScreen = false;
+        input = "";
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            input = input + "0";
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            input = input + "1";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            input = input + "2";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            input = input + "3";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            input = input + "4";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            input = input + "5";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            input = input + "6";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            input = input + "7";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            input = input + "8";
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            input = input + "9";
+        }
+
+        if (input == curPassword)
+        {
+            doorOpen = true;
+        }
+
+        if (doorOpen)
+        {
+            var newRot = Quaternion.RotateTowards(doorHinge.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 250);
+            doorHinge.rotation = newRot;
+
+            Vrzganie.Play();
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (!doorOpen)
+        {
+            if (onTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 400, 50), "'E' to open keypad");
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    keypadScreen = true;
+                    onTrigger = false;
+                }
+            }
+
+            if (keypadScreen)
+            {
+                GUI.Box(new Rect(0, 0, 640, 70), "");
+                GUI.Box(new Rect(10, 10, 620, 50), input);
+                GUI.Label(new Rect(10, 60, 200, 50), "Enter the keycode");
+                GUI.Label(new Rect(10, 75, 200, 50), "'Esc' to exit");
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+
+                    keypadScreen = false;
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Assets/Sripts/doorTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorTrigger : MonoBehaviour
+{
+
+    public Transform doorOpen;
+    public bool doorOpened;
+    public bool onTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+    void Update()
+    {
+        if (onTrigger == true)
+        {
+            doorOpened = true;
+        }
+        else
+        {
+            var newRotclosed = Quaternion.RotateTowards(doorOpen.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 100);
+            doorOpen.rotation = newRotclosed;
+            doorOpened = false;
+        }
+
+        if (doorOpened == true)
+        {
+            var newRot = Quaternion.RotateTowards(doorOpen.rotation, Quaternion.Euler(0.0f, 90.0f, 0.0f), Time.deltaTime * 100);
+            doorOpen.rotation = newRot;
+        }
+    }
+
+}
